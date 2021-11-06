55f473c40b2e279a4b7c696e3bf62e48f2afc428
Nasir Uddin <nunnasir@gmail.com>
2021-11-06 22:37:15 +0600

    update for heroku

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const e = require('express');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
