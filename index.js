13ddbca7a5696bea909431f66d4e25ffeaf55ee4
Nasir Uddin <nunnasir@gmail.com>
2021-11-06 22:29:58 +0600

    ema john server

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+const e = require('express');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.whkff.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const run = async () => {
+    try {
+        await client.connect();
+
+        const database = client.db('emaJohnShop');
+        const productCollection = database.collection('products');
+        const orderCollection = database.collection('orders');
+
+        // GET
+        app.get('/products', async (req, res) => {
+            const page = req.query.page;
+            const size = parseInt(req.query.size);
+            const cursor = productCollection.find({});
+            const count = await cursor.count();
+            let products;
+            if (page) {
+                products = await cursor.skip(page * size).limit(size).toArray();
+            }
+            else {
+                products = await cursor.toArray();
+            }
+
+            res.send({
+                count,
+                products
+            });
+        })
+
+        // USE POST TO GET DATA BY KEYS
+        app.post('/products/byKeys', async (req, res) => {
+            const keys = req.body;
+            const query = { key: { $in: keys } };
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+
+            res.send(products);
+        })
+
+        // GET:ID
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+
+            res.json(product);
+        })
+
+        // POST FOR ORDER
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+
+            res.json(result);
+        })
+
+    } finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Node are working.');
+})
+
+app.listen(port, () => {
+    console.log(`Listening are working with port: ${port}`)
+})
